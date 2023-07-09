aaa58265d4a52663fe6bf63a558ded222d64d6dd
Siddiquisohel <103947143+Siddiquisohel@users.noreply.github.com>
2023-07-09 18:02:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+ window.addEventListener('DOMContentLoaded'), (event) => {
+      const myElement = document.getElementById('myElement');
+      myElement.addEventListener('click', showAlert);
+    };
+
+    function showAlert() {
+      alert('This is an alert box!')
+    }
+</script>;
